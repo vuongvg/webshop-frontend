f38c669d5f0f3911ca2555af9c9a4df27bc5c05c
vuongvg <vuongcdt1@gmail.com>
2022-08-13 23:58:25 +0700

    deploy vercel

--- components/BannerDetail.js
+++ components/BannerDetail.js
@@ -9,13 +9,13 @@ function BannerDetail() {
        <div className="banner-content">
           <div>
              <h3>Shop The Latest Trends</h3>
-             <p>Shop the latest clothing trends with our weekly edit of what's new in online at Voxo. From out latest woman collection.</p>
+             <p>Shop the latest clothing trends with our weekly edit of what &apos;s new in online at Voxo. From out latest woman collection.</p>
           </div>
        </div>
     </div>
     <div className="banner-contain mt-3 mb-5">
        <p className="font-light">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
        </p>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
    return (
@@ -8,20 +9,20 @@ export default function Document() {
                rel="stylesheet"
                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
                integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
-               crossorigin="anonymous"
+               crossOrigin="anonymous"
                referrerpolicy="no-referrer"
             />
 
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
             <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
          </Head>
          <body>
             <Main />
             <NextScript />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css" />
-            <script src="/js/jquery-3.5.1.min.js"></script>
-            <script src="https://unpkg.com/feather-icons"></script>
+            <script src="/js/jquery-3.5.1.min.js" async></script>
+            <script src="https://unpkg.com/feather-icons" async></script>
          </body>
       </Html>
    );
